07ffa9b46bbc7e7706400706466fbaad6b6370ff
brnbp <bruno9pereira@gmail.com>
2018-08-03 15:29:18 -0300

    create config file

---
+++ config/kafka.php
@@ -0,0 +1,29 @@
+<?php
+
+
+return [
+    'brokers' => [
+        'default' => [
+            'broker' => '',
+            'auth' => [
+                'protocol' => 'ssl',
+                'ca' => storage_path('ca.pem'),
+                'certificate' => storage_path('kafka.cert'),
+                'key' => storage_path('kafka.key'),
+            ],
+        ],
+    ],
+    
+    'topics' => [
+        'default' => [
+            'topic' => 'default',
+            'broker-connection' => 'default',
+            'consumer-groups' => [
+                'default' => [
+                    'offset' => 'initial',
+                    'consumer' => '',
+                ],
+           ],
+        ],
+    ],
+];

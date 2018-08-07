0543bd1d71d690802e5a7a41620c8ac395c99129
brnbp <bruno9pereira@gmail.com>
2018-08-07 20:44:29 -0300

    create command

---
+++ src/Command.php
@@ -0,0 +1,33 @@
+<?php
+namespace Metamorphosis;
+
+use Illuminate\Console\Command as BaseCommand;
+
+class Command extends BaseCommand
+{
+    protected $name = 'kafka:consume';
+
+    protected $description = 'Consumes something';
+
+    protected $signature = 'kafka:consume
+        {topic : topic.}
+        {consumer-group : consumer group name.}
+        {--offset : Sets offset for consumer}
+        {--timeout : Sets timeout for consumer}';
+
+
+    public function handle()
+    {
+        $consumer = new Consumer($this->argument('topic'), $this->argument('consumer-group'));
+
+        if ($timeout = $this->option('timeout')) {
+            $consumer->setTimeout($timeout);
+        }
+
+        if ($offset = $this->option('offset')) {
+            $consumer->setOffset($offset);
+        }
+
+        $consumer->consume();
+    }
+}
--- src/Consumer.php
+++ src/Consumer.php
@@ -16,6 +16,16 @@ class Consumer
      */
     public $conf;
 
+    public $consumerGroup;
+
+    public $offset;
+
+    public $topic;
+
+    public $timeout = 2000000;
+
+    protected $consumer;
+
     public function __construct(string $topicKey, string $consumerGroup)
     {
         $config = new Config($topicKey,$consumerGroup);
@@ -32,19 +42,18 @@ class Consumer
         $this->conf = $this->connector->setup();
     }
 
-    public function consume()
+    public function consume(): void
     {
         $kafkaConsumer = $this->getConsumer();
-        $timeout = 2000 * 1000;
 
         $consumer = $this->consumer;
 
         while(true) {
-            $message = $kafkaConsumer->consume($timeout);
+            $message = $kafkaConsumer->consume($this->timeout);
 
             if ($message->err) {
                 echo 'error: ';
-                handleError($message);
+                $this->handleError($message);
                 echo "\n";
                 continue;
             }
@@ -53,15 +62,42 @@ class Consumer
         }
     }
 
+    public function setTimeout(int $timeout): void
+    {
+        $this->timeout = $timeout;
+    }
+
+    public function setOffset(string $offset): void
+    {
+        $this->offset = $offset;
+    }
+
     protected function getConsumer(): KafkaConsumer
     {
-        $consumerGroup = key($this->consumerConfs['consumer-groups']);
+        $consumerGroup = key($this->consumerGroup['consumer-groups']);
         $this->conf->set('group.id', $consumerGroup);
-        $this->conf->set('auto.offset.reset', $this->consumerConfs['consumer-groups']['offset']);
+        $this->conf->set('auto.offset.reset', $this->consumerGroup['consumer-groups']['offset']);
 
         $consumer = new KafkaConsumer($this->conf);
         $consumer->subscribe([$this->topic]);
 
         return $consumer;
+    }
+
+    protected function handleError($message) {
+        switch ($message->err) {
+            case RD_KAFKA_RESP_ERR_NO_ERROR:
+                var_dump($message);
+                break;
+            case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                echo "No more messages; will wait for more\n";
+                break;
+            case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                echo "Timed out\n";
+                break;
+            default:
+                var_dump($message);
+                break;
+        }
     }
 }

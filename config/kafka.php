5caebfcbb755b50b7700c3b896156677cd3e9da9
vitorbari <vitorbari@gmail.com>
2018-08-09 14:26:49 -0300

    Add basic documentation to config

--- config/kafka.php
+++ config/kafka.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Brokers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the connections details for each broker configured
+    | on topic's broker key.
+    |
+    */
 
     'brokers' => [
         'default' => [
@@ -13,6 +22,30 @@ return [
             ],
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Topics
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the configuration for the topics to be consumed.
+    |
+    | Every topic must have a unique identification key. This key must be passed
+    | as argument when using the command to consume topics.
+    |
+    | For every topic you may define the following properties:
+    |
+    | 'topic': The topic name to subscribe to
+    | 'broker': The broker identification key
+    | 'consumer-groups': You may define more than one consumer group per topic.
+    |                    If there is just one defined, it will be used by default,
+    |                    otherwise, you may pass which consumer group should be used
+    |                    when using the consumer command.
+    |                    For every consumer group, you may define:
+                         'offset': to be used as the 'auto.offset.reset'
+    |                    'consumer': a callable that will receive the message payload
+    |
+    */
 
     'topics' => [
         'default' => [

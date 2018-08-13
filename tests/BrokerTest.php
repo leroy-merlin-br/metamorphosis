7a2a71a4ceb27147fe8c45773368b6deca8a9db3
brnbp <bruno9pereira@gmail.com>
2018-08-13 20:08:25 -0300

    adds broker test

---
+++ tests/BrokerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests;
+
+use Metamorphosis\Authentication\NoAuthentication;
+use Metamorphosis\Broker;
+use RdKafka\Conf;
+
+class BrokerTest extends LaravelTestCase
+{
+    /** @test */
+    public function it_should_construct_broker_without_authentication()
+    {
+        $broker = new Broker('some-connection');
+
+        $this->assertSame('some-connection', $broker->getConnection());
+        $this->assertInstanceOf(NoAuthentication::class, $broker->getAuthentication());
+    }
+
+    /** @test */
+    public function it_should_authenticate()
+    {
+        $broker = new Broker('some-connection');
+
+        $this->assertNull($broker->authentication(new Conf()));
+    }
+}

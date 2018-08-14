25c6f30361c8470f7a2545801ac45dcb2a1a2222
vitorbari <vitorbari@gmail.com>
2018-08-14 16:53:13 -0300

    wip

--- src/Middlewares/AbstractMiddlewareHandler.php
+++ src/Middlewares/AbstractMiddlewareHandler.php
@@ -4,24 +4,10 @@ namespace Metamorphosis\Middlewares;
 use Metamorphosis\Contracts\MiddlewareHandler;
 use Metamorphosis\Message;
 
-/**
- * An abstract PSR-15 request handler.
- */
 abstract class AbstractMiddlewareHandler implements MiddlewareHandler
 {
-    /**
-     * Constructor.
-     *
-     * @param array|Traversable $queue    a queue of middleware entries
-     * @param callable          $resolver converts queue entries to middleware
-     *                                    instances
-     */
     public function __construct(iterable $queue)
     {
-        // if (!is_iterable($queue)) {
-        //     throw new TypeError('\$queue must be array or Traversable.');
-        // }
-
         $this->queue = $queue;
     }
 

ecbba42798dcc42e0e22f3a8fc13334175217136
vitorbari <vitorbari@gmail.com>
2018-08-15 10:02:19 -0300

    Add unit tests for Middlewares/Log

--- src/Middlewares/Log.php
+++ src/Middlewares/Log.php
@@ -1,16 +1,28 @@
 <?php
 namespace Metamorphosis\Middlewares;
 
-use Illuminate\Support\Facades\Log as BaseLog;
+use Illuminate\Contracts\Logging\Log as BaseLog;
 use Metamorphosis\Contracts\Middleware;
 use Metamorphosis\Contracts\MiddlewareHandler;
 use Metamorphosis\Message;
 
 class Log implements Middleware
 {
+    /**
+     * @var BaseLog
+     */
+    protected $log;
+
+    public function __construct(BaseLog $log)
+    {
+        $this->log = $log;
+    }
+
     public function process(Message $message, MiddlewareHandler $handler): void
     {
-        BaseLog::info(print_r($message, true));
+        $this->log->info('Processing kafka message: '.$message->getPayload(), [
+            'original' => (array) $message->getOriginal(),
+        ]);
 
         $handler->handle($message);
     }

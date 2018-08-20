d9746bd5663a3ec3d3e102ce9adc6ed9fe27f665
vitorbari <vitorbari@gmail.com>
2018-08-20 16:53:42 -0300

    Update broker connections key on ConsumerTest

--- tests/ConsumerTest.php
+++ tests/ConsumerTest.php
@@ -23,7 +23,7 @@ class ConsumerTest extends LaravelTestCase
             'kafka' => [
                 'brokers' => [
                     'default' => [
-                        'connection' => '',
+                        'connections' => '',
                     ],
                 ],
                 'topics' => [

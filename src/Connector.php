2dde275642babf3b0692d942f8d95f9bb4f7c523
brnbp <bruno9pereira@gmail.com>
2018-08-21 22:52:49 -0300

    minor changes for better code reading

--- src/Connector.php
+++ src/Connector.php
@@ -20,33 +20,38 @@ class Connector
     public function getConsumer(): ConsumerTopic
     {
         $conf = $this->getConf();
-
         $conf->set('group.id', $this->config->getConsumerGroupId());
 
         $broker = $this->config->getBrokerConfig();
-
         $broker->authenticate($conf);
 
         $consumer = new \RdKafka\Consumer($conf);
-
         $consumer->addBrokers($broker->getConnections());
 
-        $topicConf = new TopicConf();
+        $topicConfig = $this->getTopicConfigs();
 
-        $topicConf->set('offset.store.method', 'broker');
-        //$topicConf->set('offset.store.method', 'broker');
+        $topicConsumer = $consumer->newTopic($this->config->getTopic(), $topicConfig);
+
+        // get partition from config and offset for command output/config?
+        $topicConsumer->consumeStart(0, 4000);
+
+        return $topicConsumer;
+    }
+
+    protected function getTopicConfigs()
+    {
+        $topicConfig = new TopicConf();
+
+        $topicConfig->set('offset.store.method', 'broker');
+        //$topicConfig->set('offset.store.method', 'broker');
         // make this configurable ? config/kafka.php
 
         // Set where to start consuming messages when there is no initial offset in
         // offset store or the desired offset is out of range.
         // 'smallest': start from the beginning
-        $topicConf->set('auto.offset.reset', $this->config->getConsumerGroupOffset());
+        $topicConfig->set('auto.offset.reset', $this->config->getConsumerGroupOffset());
 
-        $topicConsumer = $consumer->newTopic($this->config->getTopic(), $topicConf);
-
-        $topicConsumer->consumeStart(0, 4000);
-
-        return $topicConsumer;
+        return $topicConfig;
     }
 
     protected function getConf(): Conf

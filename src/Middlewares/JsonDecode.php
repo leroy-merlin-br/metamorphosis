c87b2e21bc2a9f2df251ba84f38d1059296f2c15
vitorbari <vitorbari@gmail.com>
2018-09-19 17:25:10 -0300

    Add JsonDecode middleware

---
+++ src/Middlewares/JsonDecode.php
@@ -0,0 +1,24 @@
+<?php
+namespace Metamorphosis\Middlewares;
+
+use Exception;
+use Metamorphosis\Middlewares\Handler\MiddlewareHandler;
+use Metamorphosis\Record;
+
+class JsonDecode implements Middleware
+{
+    public function process(Record $record, MiddlewareHandler $handler): void
+    {
+        $payload = json_decode($record->getPayload(), true);
+
+        if ($payload === null && json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception(
+                'Malformed JSON. Error: '.json_last_error_msg()
+            );
+        }
+
+        $record->setPayload($payload);
+
+        $handler->handle($record);
+    }
+}
---
+++ tests/Middlewares/JsonDecodeTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Tests\Middlewares;
+
+use Exception;
+use Metamorphosis\Middlewares\Handler\Iterator;
+use Metamorphosis\Middlewares\JsonDecode;
+use Metamorphosis\Record;
+use RdKafka\Message as KafkaMessage;
+use Tests\LaravelTestCase;
+
+class JsonDecodeTest extends LaravelTestCase
+{
+    /** @test */
+    public function it_should_decode_and_update_message_payload()
+    {
+        $data = [['member_id' => 1392, 'member_name' => 'Jose']];
+
+        $json = json_encode($data);
+
+        $middleware = new JsonDecode();
+
+        $kafkaMessage = new KafkaMessage();
+        $kafkaMessage->payload = $json;
+        $kafkaMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+
+        $record = new Record($kafkaMessage);
+
+        $handler = $this->createMock(Iterator::class);
+
+        $handler->expects($this->once())
+            ->method('handle')
+            ->with($this->equalTo($record));
+
+        $middleware->process($record, $handler);
+
+        $this->assertSame($data, $record->getPayload());
+    }
+
+    /** @test */
+    public function it_should_throw_an_exception_on_invalid_json_string()
+    {
+        $json = "{'Organization': 'Metamorphosis Team'}";
+
+        $middleware = new JsonDecode();
+
+        $kafkaMessage = new KafkaMessage();
+        $kafkaMessage->payload = $json;
+        $kafkaMessage->err = RD_KAFKA_RESP_ERR_NO_ERROR;
+
+        $record = new Record($kafkaMessage);
+
+        $handler = $this->createMock(Iterator::class);
+
+        $handler->expects($this->never())
+            ->method('handle');
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Malformed JSON. Error: Syntax error');
+
+        $middleware->process($record, $handler);
+    }
+}

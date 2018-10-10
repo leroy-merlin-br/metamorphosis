bfb95b58047946d893a0c7a7c08534ff56c28264
brnbp <bruno9pereira@gmail.com>
2018-10-10 20:53:21 -0300

    tests producer connector with topic handler

--- tests/Connectors/Producer/ConnectorTest.php
+++ tests/Connectors/Producer/ConnectorTest.php
@@ -3,6 +3,9 @@ namespace Tests\Connectors\Producer;
 
 use Metamorphosis\Config\Producer;
 use Metamorphosis\Connectors\Producer\Connector;
+use Metamorphosis\TopicHandler\Producer\HandleableResponse;
+use Metamorphosis\TopicHandler\Producer\AbstractHandler;
+use RdKafka\Message;
 use RdKafka\ProducerTopic;
 use Tests\LaravelTestCase;
 
@@ -14,6 +17,34 @@ class ConnectorTest extends LaravelTestCase
         $config = $this->createMock(Producer::class);
 
         $connector = new Connector();
+
+        $producer = $connector->getProducerTopic($config);
+
+        $this->assertInstanceOf(ProducerTopic::class, $producer);
+    }
+
+    /** @test */
+    public function it_should_make_setup_with_topic_handler()
+    {
+        $config = $this->createMock(Producer::class);
+
+        $handler = new class('record', 'some-topic') extends AbstractHandler implements HandleableResponse {
+            public function __construct($record, string $topic, ?string $key = null, ?int $partition = null)
+            {
+            }
+
+            public function success(Message $message): void
+            {
+            }
+
+            public function failed(Message $message): void
+            {
+            }
+        };
+
+        $connector = new Connector();
+
+        $connector->setHandler($handler);
 
         $producer = $connector->getProducerTopic($config);
 

fcf70fbbf0a6ae4ffd91ddc622c44044ccef6d5c
brnbp <bruno9pereira@gmail.com>
2018-10-10 21:12:51 -0300

    tests producer connector with topic handler

--- tests/Connectors/Producer/ConnectorTest.php
+++ tests/Connectors/Producer/ConnectorTest.php
@@ -3,6 +3,7 @@ namespace Tests\Connectors\Producer;
 
 use Metamorphosis\Config\Producer;
 use Metamorphosis\Connectors\Producer\Connector;
+use Metamorphosis\Connectors\Producer\Queue;
 use Metamorphosis\TopicHandler\Producer\HandleableResponse;
 use Metamorphosis\TopicHandler\Producer\AbstractHandler;
 use RdKafka\Message;
@@ -48,6 +49,44 @@ class ConnectorTest extends LaravelTestCase
 
         $producer = $connector->getProducerTopic($config);
 
+        $this->assertInstanceOf(ProducerTopic::class, $producer);
+    }
+
+    /** @test */
+    public function it_should_handle_response_from_broker()
+    {
+        $config = $this->createMock(Producer::class);
+        $queue = $this->createMock(Queue::class);
+
+        $handler = new class('record', 'some-topic') extends AbstractHandler implements HandleableResponse {
+            public function __construct($record, string $topic, ?string $key = null, ?int $partition = null)
+            {
+            }
+
+            public function success(Message $message): void
+            {
+            }
+
+            public function failed(Message $message): void
+            {
+            }
+        };
+
+        $config->expects($this->once())
+            ->method('getTimeoutResponse')
+            ->willReturn(50);
+
+        $connector = new Connector();
+
+        $connector->setHandler($handler);
+
+        $connector->queue = $queue;
+
+        $producer = $connector->getProducerTopic($config);
+
+        $nullReturn = $connector->handleResponsesFromBroker();
+
+        $this->assertNull($nullReturn);
         $this->assertInstanceOf(ProducerTopic::class, $producer);
     }
 }

7a701360ef80baf2aa161bbc2f2ce0fe11070b56
Gabriel Moreira <gabrielemidiomoreira@gmail.com>
2019-06-18 15:42:01 -0300

    fix consumer make command test

--- tests/Console/ConsumerMakeCommandTest.php
+++ tests/Console/ConsumerMakeCommandTest.php
@@ -1,19 +1,19 @@
 <?php
 namespace Tests\Console;
 
+use Illuminate\Support\Facades\Artisan;
 use Tests\LaravelTestCase;
 
 class ConsumerMakeCommandTest extends LaravelTestCase
 {
     public function testItShouldGenerateConsumerHandler()
     {
-        $this->markTestSkipped();
         $command = 'make:kafka-consumer';
         $parameters = [
             'name' => str_random(8),
         ];
 
-        $statusCode = $this->artisan($command, $parameters);
+        $statusCode = Artisan::call($command, $parameters);
 
         $this->assertSame(0, $statusCode);
     }

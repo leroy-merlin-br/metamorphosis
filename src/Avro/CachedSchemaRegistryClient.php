8624d019e21dd5641cc084b362f055cff8c791ff
djonasm <davidjonasmarques@gmail.com>
2019-12-05 20:21:20 -0300

    add avcro schema client and message serializer

--- src/Avro/CachedSchemaRegistryClient.php
+++ src/Avro/CachedSchemaRegistryClient.php
@@ -1,10 +1,260 @@
 <?php
-
-
 namespace Metamorphosis\Avro;
 
+use AvroSchema;
+use GuzzleHttp\Client;
+use RuntimeException;
 
 class CachedSchemaRegistryClient
 {
+    private $maxSchemasPerSubject;
 
+    private $client;
+
+    /* Schemas by Ids */
+    /**
+     * @var SplObjectStorage[]|int[][]
+     */
+    private $subjectToSchemaIds = [];
+
+    /**
+     * @var AvroSchema[]
+     */
+    private $idToSchema = [];
+
+    /**
+     * @var SplObjectStorage[]|int[][]
+     */
+    private $subjectToSchemaVersions = [];
+
+    /* Schemas by Version */
+    /**
+     * @var AvroSchema[][]
+     */
+    private $subjectVersionToSchema = [];
+
+    /**
+     * @param string $url
+     * @param int    $maxSchemasPerSubject
+     */
+    public function __construct($url, $maxSchemasPerSubject = 1000)
+    {
+        $this->maxSchemasPerSubject = $maxSchemasPerSubject;
+
+        $this->client = new Client(['base_uri' => $url, 'timeout' => 40000]);
+    }
+
+    /**
+     * POST /subjects/(string: subject)/versions
+     * Register a schema with the registry under the given subject
+     * and receive a schema id.
+     *
+     * $schema must be a parsed schema from the php avro library
+     *
+     * Multiple instances of the same schema will result in cache misses.
+     *
+     * @param string     $subject Subject name
+     * @param AvroSchema $schema  Avro schema to be registered
+     *
+     * @return int
+     */
+    public function register($subject, AvroSchema $schema)
+    {
+        if (isset($this->subjectToSchemaIds[$subject])) {
+            $schemasToId = $this->subjectToSchemaIds[$subject];
+
+            if (isset($schemasToId[$schema])) {
+                return $schemasToId[$schema];
+            }
+        }
+
+        $url = sprintf('/subjects/%s/versions', $subject.'-value');
+        [$status, $response] = $this->sendRequest($url, 'POST', json_encode(['schema' => (string) $schema]));
+
+        if (409 === $status) {
+            throw new RuntimeException('Incompatible Avro schema');
+        } elseif (422 === $status) {
+            throw new RuntimeException('Invalid Avro schema');
+        } elseif (!($status >= 200 || $status < 300)) {
+            throw new RuntimeException('Unable to register schema. Error code: '.$status);
+        }
+
+        $schemaId = $response['id'];
+        $this->cacheSchema($schema, $schemaId, $subject);
+
+        return $schemaId;
+    }
+
+    /**
+     * Returns the version of a registered schema
+     *
+     * @param string $subject
+     *
+     * @return int
+     */
+    public function getSchemaVersion($subject, AvroSchema $schema)
+    {
+        if (!isset($this->subjectToSchemaVersions[$subject][$schema])) {
+            $this->cacheSchemaDetails($subject, $schema);
+        }
+
+        return $this->subjectToSchemaVersions[$subject][$schema];
+    }
+
+    /**
+     * Returns the id of a registered schema
+     *
+     * @param string $subject
+     *
+     * @return int
+     */
+    public function getSchemaId($subject, AvroSchema $schema)
+    {
+        if (!isset($this->subjectToSchemaVersions[$subject][$schema])) {
+            $this->cacheSchemaDetails($subject, $schema);
+        }
+
+        return $this->subjectToSchemaIds[$subject][$schema];
+    }
+
+    /**
+     * Fetch and caches the details of a schema
+     *
+     * @param string $subject
+     */
+    protected function cacheSchemaDetails($subject, AvroSchema $schema)
+    {
+        $url = sprintf('/subjects/%s', $subject);
+        [$status, $response] = $this->sendRequest($url, 'POST', json_encode(['schema' => (string) $schema]));
+        if (!($status >= 200 || $status < 300)) {
+            throw new RuntimeException('Unable to get schema details. Error code: '.$status);
+        }
+
+        $response['schema'] = $schema;
+
+        $this->cacheSchema($response['schema'], $response['id'], $response['subject'], $response['version']);
+    }
+
+    /**
+     * GET /schemas/ids/{int: id}
+     * Retrieve a parsed avro schema by id or None if not found
+     *
+     * @param int $schemaId value
+     *
+     * @return AvroSchema Avro schema
+     */
+    public function getById($schemaId)
+    {
+        if (isset($this->idToSchema[$schemaId])) {
+            return $this->idToSchema[$schemaId];
+        }
+
+        $url = sprintf('/schemas/ids/%d', $schemaId);
+        [$status, $response] = $this->sendRequest($url, 'GET');
+
+        if (404 === $status) {
+            throw new RuntimeException('Schema not found');
+        } elseif (!($status >= 200 || $status < 300)) {
+            throw new RuntimeException('Unable to get schema for the specific ID: '.$status);
+        }
+
+        $schema = AvroSchema::parse($response['schema']);
+
+        $this->cacheSchema($schema, $schemaId);
+
+        return $schema;
+    }
+
+    public function getBySubjectAndVersion($subject, $version)
+    {
+        if (isset($this->subjectVersionToSchema[$subject][$version])) {
+            return $this->subjectVersionToSchema[$subject][$version];
+        }
+
+        $url = sprintf('/subjects/%s/versions/%d', $subject, $version);
+        [$status, $response] = $this->sendRequest($url, 'GET');
+
+        if (404 === $status) {
+            throw new RuntimeException('Schema not found');
+        } elseif (!($status >= 200 || $status < 300)) {
+            throw new RuntimeException('Unable to get schema for the specific ID: '.$status);
+        }
+
+        $schema = AvroSchema::parse($response['schema']);
+
+        $this->cacheSchemaDetails($subject, $schema);
+
+        return $schema;
+    }
+
+    private function sendRequest($url, $method = 'GET', $body = null, $headers = null)
+    {
+        $headers = (array) $headers;
+        $headers['Accept'] = 'application/vnd.schemaregistry.v1+json, application/vnd.schemaregistry+json, application/json';
+
+        if ($body) {
+            $headers['Content-Type'] = 'application/vnd.schemaregistry.v1+json';
+        }
+
+        switch ($method) {
+            case 'GET':
+                $response = $this->client->get($url, $headers);
+                break;
+            case 'POST':
+                $response = $this->client->post($url, $headers, $body);
+                break;
+            case 'PUT':
+                $response = $this->client->put($url, $headers, $body);
+                break;
+            case 'DELETE':
+                $response = $this->client->delete($url, $headers);
+                break;
+            default:
+                throw new RuntimeException('Invalid HTTP method');
+        }
+
+        return [$response->getStatusCode(), json_decode($response->getBody(true), true)];
+    }
+
+    /**
+     * @param int         $schemaId
+     * @param string|null $subject
+     * @param string|null $version
+     */
+    private function cacheSchema(AvroSchema $schema, $schemaId, $subject = null, $version = null)
+    {
+        if (isset($this->idToSchema[$schemaId])) {
+            $schema = $this->idToSchema[$schemaId];
+        } else {
+            $this->idToSchema[$schemaId] = $schema;
+        }
+
+        if ($subject) {
+            $this->addToCache($this->subjectToSchemaIds, $subject, $schema, $schemaId);
+
+            if ($version) {
+                if (!isset($this->subjectVersionToSchema[$subject])) {
+                    $this->subjectVersionToSchema[$subject] = [];
+                }
+
+                $this->subjectVersionToSchema[$subject][$version] = $schema;
+
+                $this->addToCache($this->subjectToSchemaVersions, $subject, $schema, $version);
+            }
+        }
+    }
+
+    /**
+     * @param \SplObjectStorage[] $cache
+     * @param string              $subject
+     * @param string              $value
+     */
+    private function addToCache(&$cache, $subject, AvroSchema $schema, $value)
+    {
+        if (!isset($cache[$subject])) {
+            $cache[$subject] = new SplObjectStorage();
+        }
+
+        $cache[$subject][$schema] = $value;
+    }
 }
--- src/Avro/MessageSerializer.php
+++ src/Avro/MessageSerializer.php
@@ -1,10 +1,280 @@
 <?php
-
-
 namespace Metamorphosis\Avro;
 
+use AvroIO;
+use AvroIOBinaryDecoder;
+use AvroIOBinaryEncoder;
+use AvroIODatumReader;
+use AvroIODatumWriter;
+use AvroSchema;
+use AvroStringIO;
+use RuntimeException;
 
 class MessageSerializer
 {
+    const MAGIC_BYTE_SCHEMAID = 0;
+    const MAGIC_BYTE_SUBJECT_VERSION = 1;
 
+    private $idToDecoderFunc = [];
+    private $subjectVersionToDecoderFunc = [];
+
+    /**
+     * @var AvroIODatumWriter[]
+     */
+    private $idToWriters = [];
+
+    /**
+     * @var AvroIODatumWriter[][]
+     */
+    private $subjectVersionToWriters = [];
+
+    /**
+     * @var CachedSchemaRegistryClient
+     */
+    private $registry;
+
+    private $registerMissingSchemas = false;
+    private $defaultEncodingFormat = self::MAGIC_BYTE_SCHEMAID;
+
+    public function __construct(CachedSchemaRegistryClient $registry, $options = [])
+    {
+        $this->registry = $registry;
+
+        if (isset($options['register_missing_schemas'])) {
+            $this->registerMissingSchemas = $options['register_missing_schemas'];
+        }
+
+        if (isset($options['default_encoding_format'])) {
+            $this->defaultEncodingFormat = $options['default_encoding_format'];
+        }
+    }
+
+    /**
+     * Encode a record with a given schema id.
+     *
+     * @param int   $schemaId
+     * @param array $record   A data to serialize
+     * @param bool  $isKey    If the record is a key
+     *
+     * @return AvroIODatumWriter encoder object
+     */
+    public function encodeRecordWithSchemaId($schemaId, array $record, $isKey = false)
+    {
+        if (!isset($this->idToWriters[$schemaId])) {
+            $schema = $this->registry->getById($schemaId);
+
+            $this->idToWriters[$schemaId] = new AvroIODatumWriter($schema);
+        }
+
+        $writer = $this->idToWriters[$schemaId];
+
+        $io = new AvroStringIO();
+
+        // write the header
+
+        // magic byte
+        $io->write(pack('C', static::MAGIC_BYTE_SCHEMAID));
+
+        // write the schema ID in network byte order (big end)
+        $io->write(pack('N', $schemaId));
+
+        // write the record to the rest of it
+        // Create an encoder that we'll write to
+        $encoder = new AvroIOBinaryEncoder($io);
+
+        // write the object in 'obj' as Avro to the fake file...
+        $writer->write($record, $encoder);
+
+        return $io->string();
+    }
+
+    /**
+     * Encode a record with a given schema id.
+     *
+     * @param string $subject
+     * @param int    $version
+     * @param array  $record  A data to serialize
+     * @param bool   $isKey   If the record is a key
+     *
+     * @return AvroIODatumWriter encoder object
+     */
+    public function encodeRecordWithSubjectAndVersion($subject, $version, array $record, $isKey = false)
+    {
+        if (!isset($this->subjectVersionToWriters[$subject][$version])) {
+            $schema = $this->registry->getBySubjectAndVersion($subject, $version);
+
+            $this->subjectVersionToWriters[$subject][$version] = new AvroIODatumWriter($schema);
+        }
+
+        $writer = $this->subjectVersionToWriters[$subject][$version];
+
+        $io = new AvroStringIO();
+
+        // write the header
+
+        // magic byte
+        $io->write(pack('C', static::MAGIC_BYTE_SUBJECT_VERSION));
+
+        // write the subject length in network byte order (big end)
+        $io->write(pack('N', strlen($subject)));
+
+        // then the subject
+        foreach (str_split($subject) as $letter) {
+            $io->write(pack('C', ord($letter)));
+        }
+
+        // and finally the version
+        $io->write(pack('N', $version));
+
+        // write the record to the rest of it
+        // Create an encoder that we'll write to
+        $encoder = new AvroIOBinaryEncoder($io);
+
+        // write the object in 'obj' as Avro to the fake file...
+        $writer->write($record, $encoder);
+
+        return $io->string();
+    }
+
+    /**
+     * Given a parsed avro schema, encode a record for the given topic.
+     * The schema is registered with the subject of 'topic-value'
+     *
+     * @param string     $topic  Topic name
+     * @param AvroSchema $schema Avro Schema
+     * @param array      $record An object to serialize
+     * @param bool       $isKey  If the record is a key
+     *
+     * @return string Encoded record with schema ID as bytes
+     */
+    public function encodeRecordWithSchema($topic, AvroSchema $schema, array $record, $isKey = false, $format = null)
+    {
+        $suffix = $isKey ? '-key' : '-value';
+        $subject = $topic.$suffix;
+
+        $format = $format ?? $this->defaultEncodingFormat;
+
+        switch ($format) {
+            case self::MAGIC_BYTE_SUBJECT_VERSION:
+                try {
+                    $version = $this->registry->getSchemaVersion($subject, $schema);
+                } catch (RuntimeException $e) {
+                    if ($this->registerMissingSchemas) {
+                        $this->registry->register($subject, $schema);
+                        $version = $this->registry->getSchemaVersion($subject, $schema);
+                    } else {
+                        throw $e;
+                    }
+                }
+
+                $this->subjectVersionToWriters[$subject][$version] = new AvroIODatumWriter($schema);
+
+                return $this->encodeRecordWithSubjectAndVersion($subject, $version, $record, $isKey);
+                break;
+            case self::MAGIC_BYTE_SCHEMAID:
+                try {
+                    $id = $this->registry->getSchemaId($subject, $schema);
+                } catch (RuntimeException $e) {
+                    if ($this->registerMissingSchemas) {
+                        $this->registry->register($subject, $schema);
+                        $id = $this->registry->getSchemaId($subject, $schema);
+                    } else {
+                        throw $e;
+                    }
+                }
+
+                $this->idToWriters[$id] = new AvroIODatumWriter($schema);
+
+                return $this->encodeRecordWithSchemaId($id, $record, $isKey);
+                break;
+            default:
+                throw new RuntimeException('Unsuported format: '.$format);
+        }
+    }
+
+    /**
+     * Decode a message from kafka that has been encoded for use with the schema registry.
+     *
+     * @param string $message
+     *
+     * @return array
+     * @throws \AvroIOException
+     */
+    public function decodeMessage($message)
+    {
+        if (strlen($message) < 1) {
+            throw new RuntimeException('Message is too small to decode');
+        }
+
+        $io = new AvroStringIO($message);
+
+        $magic = unpack('C', $io->read(1));
+        $magic = $magic[1];
+
+        switch ($magic) {
+            case static::MAGIC_BYTE_SCHEMAID:
+                $id = unpack('N', $io->read(4));
+                $id = $id[1];
+
+                $decoder = $this->getDecoderById($id);
+                break;
+            case static::MAGIC_BYTE_SUBJECT_VERSION:
+                $size = $io->read(4);
+                $subjectSize = unpack('N', $size);
+                $subjectBytes = unpack('C*', $io->read($subjectSize[1]));
+                $version = unpack('N', $io->read(4));
+
+                $version = $version[1];
+
+                $subject = '';
+                foreach ($subjectBytes as $subjectByte) {
+                    $subject .= chr($subjectByte);
+                }
+
+                $decoder = $this->getDecoderBySubjectAndVersion($subject, $version);
+                break;
+            default:
+                return $message;
+        }
+
+        return $decoder($io);
+    }
+
+    private function getDecoderById($schemaId)
+    {
+        if (isset($this->idToDecoderFunc[$schemaId])) {
+            return $this->idToDecoderFunc[$schemaId];
+        }
+
+        $schema = $this->registry->getById($schemaId);
+
+        $reader = new AvroIODatumReader($schema);
+
+        $this->idToDecoderFunc[$schemaId] = function (AvroIO $io) use ($reader) {
+            return $reader->read(new AvroIOBinaryDecoder($io));
+        };
+
+        return $this->idToDecoderFunc[$schemaId];
+    }
+
+    private function getDecoderBySubjectAndVersion($subject, $version)
+    {
+        if (isset($this->subjectVersionToDecoderFunc[$subject][$version])) {
+            return $this->subjectVersionToDecoderFunc[$subject][$version];
+        }
+
+        $schema = $this->registry->getBySubjectAndVersion($subject, $version);
+
+        $reader = new AvroIODatumReader($schema);
+
+        if (!isset($this->subjectVersionToDecoderFunc[$subject])) {
+            $this->subjectVersionToDecoderFunc[$subject] = [];
+        }
+
+        $this->subjectVersionToDecoderFunc[$subject][$version] = function (AvroIO $io) use ($reader) {
+            return $reader->read(new AvroIOBinaryDecoder($io));
+        };
+
+        return $this->subjectVersionToDecoderFunc[$subject][$version];
+    }
 }

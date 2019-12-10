8e3887de465d94339b01cb46fd8d049f087bfa6c
Ravan Scafi <rscafi@leroymerlin.com.br>
2019-12-10 11:55:53 -0300

    Starting to refactor Message Serializer

--- src/Avro/MessageSerializer.php
+++ src/Avro/MessageSerializer.php
@@ -8,14 +8,23 @@ use AvroIODatumReader;
 use AvroIODatumWriter;
 use AvroSchema;
 use AvroStringIO;
+use Closure;
 use RuntimeException;
 
 class MessageSerializer
 {
     const MAGIC_BYTE_SCHEMAID = 0;
+
     const MAGIC_BYTE_SUBJECT_VERSION = 1;
 
+    /**
+     * @var array
+     */
     private $idToDecoderFunc = [];
+
+    /**
+     * @var array
+     */
     private $subjectVersionToDecoderFunc = [];
 
     /**
@@ -33,88 +42,144 @@ class MessageSerializer
      */
     private $registry;
 
-    private $registerMissingSchemas = false;
-    private $defaultEncodingFormat = self::MAGIC_BYTE_SCHEMAID;
+    /**
+     * @var bool
+     */
+    private $registerMissingSchemas;
 
-    public function __construct(CachedSchemaRegistryClient $registry, $options = [])
+    /**
+     * @var int
+     */
+    private $defaultEncodingFormat;
+
+    public function __construct(CachedSchemaRegistryClient $registry, array $options = [])
     {
         $this->registry = $registry;
 
-        if (isset($options['register_missing_schemas'])) {
-            $this->registerMissingSchemas = $options['register_missing_schemas'];
-        }
-
-        if (isset($options['default_encoding_format'])) {
-            $this->defaultEncodingFormat = $options['default_encoding_format'];
-        }
+        $this->registerMissingSchemas = $options['register_missing_schemas'] ?? false;
+        $this->defaultEncodingFormat = $options['default_encoding_format'] ?? static::MAGIC_BYTE_SCHEMAID;
     }
 
     /**
-     * Given a parsed avro schema, encode a record for the given topic.
+     * Given a parsed Avro schema, encode a record for the given topic.
      * The schema is registered with the subject of 'topic-value'
      *
      * @param string     $topic  Topic name
      * @param AvroSchema $schema Avro Schema
-     * @param mixed      $record An object to serialize
+     * @param mixed      $record An record (object, array, string, etc) to serialize
      * @param bool       $isKey  If the record is a key
+     * @param int|null   $format Encoding Format
      *
+     * @throws \AvroIOException
      * @return string Encoded record with schema ID as bytes
      */
-    public function encodeRecordWithSchema($topic, AvroSchema $schema, $record, $isKey = false, $format = null)
-    {
+    public function encodeRecordWithSchema(
+        string $topic,
+        AvroSchema $schema,
+        $record,
+        bool $isKey = false,
+        int $format = null
+    ): string {
         $suffix = $isKey ? '-key' : '-value';
         $subject = $topic.$suffix;
 
         $format = $format ?? $this->defaultEncodingFormat;
 
-        switch ($format) {
-            case self::MAGIC_BYTE_SUBJECT_VERSION:
-                try {
+        if ($format === static::MAGIC_BYTE_SUBJECT_VERSION) {
+            try {
+                $version = $this->registry->getSchemaVersion($subject, $schema);
+            } catch (RuntimeException $e) {
+                if ($this->registerMissingSchemas) {
+                    $this->registry->register($subject, $schema);
                     $version = $this->registry->getSchemaVersion($subject, $schema);
-                } catch (RuntimeException $e) {
-                    if ($this->registerMissingSchemas) {
-                        $this->registry->register($subject, $schema);
-                        $version = $this->registry->getSchemaVersion($subject, $schema);
-                    } else {
-                        throw $e;
-                    }
+                } else {
+                    throw $e;
                 }
+            }
 
-                $this->subjectVersionToWriters[$subject][$version] = new AvroIODatumWriter($schema);
+            $this->subjectVersionToWriters[$subject][$version] = new AvroIODatumWriter($schema);
 
-                return $this->encodeRecordWithSubjectAndVersion($subject, $version, $record, $isKey);
-                break;
-            case self::MAGIC_BYTE_SCHEMAID:
-                try {
-                    $id = $this->registry->getSchemaId($subject, $schema);
-                } catch (RuntimeException $e) {
-                    if ($this->registerMissingSchemas) {
-                        $this->registry->register($subject, $schema);
-                        $id = $this->registry->getSchemaId($subject, $schema);
-                    } else {
-                        throw $e;
-                    }
-                }
-
-                $this->idToWriters[$id] = new AvroIODatumWriter($schema);
-
-                return $this->encodeRecordWithSchemaId($id, $record, $isKey);
-                break;
-            default:
-                throw new RuntimeException('Unsuported format: '.$format);
+            return $this->encodeRecordWithSubjectAndVersion($subject, $version, $record);
         }
+
+        if ($format === static::MAGIC_BYTE_SCHEMAID) {
+            try {
+                $id = $this->registry->getSchemaId($subject, $schema);
+            } catch (RuntimeException $e) {
+                if ($this->registerMissingSchemas) {
+                    $this->registry->register($subject, $schema);
+                    $id = $this->registry->getSchemaId($subject, $schema);
+                } else {
+                    throw $e;
+                }
+            }
+
+            $this->idToWriters[$id] = new AvroIODatumWriter($schema);
+
+            return $this->encodeRecordWithSchemaId($id, $record);
+        }
+
+        throw new RuntimeException('Unsuported format: '.$format);
+    }
+
+    /**
+     * Decode a message from kafka that has been encoded for use with the schema registry.
+     *
+     * @param string $message
+     *
+     * @return mixed
+     * @throws \AvroIOException
+     */
+    public function decodeMessage(string $message)
+    {
+        if (!$message) {
+            throw new RuntimeException('Message is too small to decode');
+        }
+
+        $io = new AvroStringIO($message);
+
+        $magicByte = unpack('C', $io->read(1));
+        $magicByte = $magicByte[1];
+
+        if ($magicByte === static::MAGIC_BYTE_SCHEMAID) {
+            $id = unpack('N', $io->read(4));
+            $id = $id[1];
+
+            $decoder = $this->getDecoderById($id);
+
+            return $decoder($io);
+        }
+
+        if ($magicByte === static::MAGIC_BYTE_SUBJECT_VERSION) {
+            $size = $io->read(4);
+            $subjectSize = unpack('N', $size);
+            $subjectBytes = unpack('C*', $io->read($subjectSize[1]));
+            $version = unpack('N', $io->read(4));
+
+            $version = $version[1];
+
+            $subject = '';
+            foreach ($subjectBytes as $subjectByte) {
+                $subject .= chr($subjectByte);
+            }
+
+            $decoder = $this->getDecoderBySubjectAndVersion($subject, $version);
+
+            return $decoder($io);
+        }
+
+        return $message;
     }
 
     /**
      * Encode a record with a given schema id.
      *
      * @param int   $schemaId
-     * @param array $record   A data to serialize
-     * @param bool  $isKey    If the record is a key
+     * @param mixed $record A data to serialize
      *
-     * @return AvroIODatumWriter encoder object
+     * @throws \AvroIOException
      */
-    private function encodeRecordWithSchemaId($schemaId, array $record, $isKey = false)
+    private function encodeRecordWithSchemaId(int $schemaId, $record): string
     {
         $writer = $this->idToWriters[$schemaId];
 
@@ -143,12 +208,11 @@ class MessageSerializer
      *
      * @param string $subject
      * @param int    $version
-     * @param array  $record  A data to serialize
-     * @param bool   $isKey   If the record is a key
+     * @param mixed  $record A data to serialize
      *
-     * @return AvroIODatumWriter encoder object
+     * @throws \AvroIOException
      */
-    private function encodeRecordWithSubjectAndVersion($subject, $version, $record, $isKey = false)
+    private function encodeRecordWithSubjectAndVersion(string $subject, int $version, $record): string
     {
         $writer = $this->subjectVersionToWriters[$subject][$version];
 
@@ -180,88 +244,36 @@ class MessageSerializer
         return $io->string();
     }
 
-    /**
-     * Decode a message from kafka that has been encoded for use with the schema registry.
-     *
-     * @param string $message
-     *
-     * @return array
-     * @throws \AvroIOException
-     */
-    public function decodeMessage($message)
+    private function getDecoderById(int $schemaId): Closure
     {
-        if (strlen($message) < 1) {
-            throw new RuntimeException('Message is too small to decode');
+        if (!isset($this->idToDecoderFunc[$schemaId])) {
+            $schema = $this->registry->getById($schemaId);
+
+            $reader = new AvroIODatumReader($schema);
+
+            $this->idToDecoderFunc[$schemaId] = function (AvroIO $io) use ($reader) {
+                return $reader->read(new AvroIOBinaryDecoder($io));
+            };
         }
-
-        $io = new AvroStringIO($message);
-
-        $magic = unpack('C', $io->read(1));
-        $magic = $magic[1];
-
-        switch ($magic) {
-            case static::MAGIC_BYTE_SCHEMAID:
-                $id = unpack('N', $io->read(4));
-                $id = $id[1];
-
-                $decoder = $this->getDecoderById($id);
-                break;
-            case static::MAGIC_BYTE_SUBJECT_VERSION:
-                $size = $io->read(4);
-                $subjectSize = unpack('N', $size);
-                $subjectBytes = unpack('C*', $io->read($subjectSize[1]));
-                $version = unpack('N', $io->read(4));
-
-                $version = $version[1];
-
-                $subject = '';
-                foreach ($subjectBytes as $subjectByte) {
-                    $subject .= chr($subjectByte);
-                }
-
-                $decoder = $this->getDecoderBySubjectAndVersion($subject, $version);
-                break;
-            default:
-                return $message;
-        }
-
-        return $decoder($io);
-    }
-
-    private function getDecoderById($schemaId)
-    {
-        if (isset($this->idToDecoderFunc[$schemaId])) {
-            return $this->idToDecoderFunc[$schemaId];
-        }
-
-        $schema = $this->registry->getById($schemaId);
-
-        $reader = new AvroIODatumReader($schema);
-
-        $this->idToDecoderFunc[$schemaId] = function (AvroIO $io) use ($reader) {
-            return $reader->read(new AvroIOBinaryDecoder($io));
-        };
 
         return $this->idToDecoderFunc[$schemaId];
     }
 
-    private function getDecoderBySubjectAndVersion($subject, $version)
+    private function getDecoderBySubjectAndVersion(string $subject, int $version): Closure
     {
-        if (isset($this->subjectVersionToDecoderFunc[$subject][$version])) {
-            return $this->subjectVersionToDecoderFunc[$subject][$version];
+        if (!isset($this->subjectVersionToDecoderFunc[$subject][$version])) {
+            $schema = $this->registry->getBySubjectAndVersion($subject, $version);
+
+            $reader = new AvroIODatumReader($schema);
+
+            if (!isset($this->subjectVersionToDecoderFunc[$subject])) {
+                $this->subjectVersionToDecoderFunc[$subject] = [];
+            }
+
+            $this->subjectVersionToDecoderFunc[$subject][$version] = function (AvroIO $io) use ($reader) {
+                return $reader->read(new AvroIOBinaryDecoder($io));
+            };
         }
-
-        $schema = $this->registry->getBySubjectAndVersion($subject, $version);
-
-        $reader = new AvroIODatumReader($schema);
-
-        if (!isset($this->subjectVersionToDecoderFunc[$subject])) {
-            $this->subjectVersionToDecoderFunc[$subject] = [];
-        }
-
-        $this->subjectVersionToDecoderFunc[$subject][$version] = function (AvroIO $io) use ($reader) {
-            return $reader->read(new AvroIOBinaryDecoder($io));
-        };
 
         return $this->subjectVersionToDecoderFunc[$subject][$version];
     }

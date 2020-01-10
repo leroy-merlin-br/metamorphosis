418936aec8073a2b175b231d1de4d17872f7ddf9
Diego Felix <diegoflx.oliveira@gmail.com>
2020-01-10 08:31:19 -0300

    refactor tests to be more readable

--- tests/Integration/ProducerTest.php
+++ tests/Integration/ProducerTest.php
@@ -13,87 +13,150 @@ class ProducerTest extends LaravelTestCase
     protected function setUp()
     {
         parent::setUp();
+        $this->withoutAuthentication();
+    }
+
+    public function testShouldRunAProducerAndReceiveMessagesWithAHighLevelConsumer(): void
+    {
+        // Given That I
+        $this->haveAConsumerHandlerConfigured();
+        $recordMessage = $this->haveSomeRandomMessagesProduced();
+
+        // I Expect That
+        $this->myMessagesHaveBeenLogged($recordMessage);
+
+        // When I
+        $this->runTheConsumer();
+    }
+
+    public function testShouldRunAProducerAndReceiveMessagesWithALowLevelConsumer(): void
+    {
+        // Given That I
+        $firstMessage = 'First Message';
+        $secondMessage = 'Second Message';
+
+        $this->haveALowLevelConsumerConfigured();
+        $this->haveTwoMessagesProducedBefore($firstMessage);
+        $this->andHaveMoreTwoMessagesProducedLater($secondMessage);
+
+        // I Expect That
+        $this->mySecondMessageHaveBeenLogged($secondMessage);
+
+        // When I
+        $this->runTheLowLevelConsumer();
+    }
+
+    protected function withoutAuthentication(): void
+    {
         config(['kafka.brokers.default.auth' => []]);
     }
 
-    public function testShouldRunProducerWithHighLevelConsumer(): void
+    protected function haveAConsumerHandlerConfigured(): void
     {
-        // Set
         config(['kafka.topics.default.consumer_groups.test-consumer-group.handler' => ProductConsumer::class]);
+    }
+
+    protected function runTheConsumer(): void
+    {
+        $this->artisan(
+            'kafka:consume',
+            [
+                'topic' => 'default',
+                'consumer_group' => 'test-consumer-group',
+                '--timeout' => 20000,
+                '--times' => 2,
+            ]
+        );
+    }
+
+    protected function haveALowLevelConsumerConfigured(): void
+    {
+        config(
+            [
+                'kafka.topics' => [
+                    'low_level' => [
+                        'topic_id' => 'low_level',
+                        'broker' => 'default',
+                        'consumer_groups' => [
+                            'test-consumer-group' => [
+                                'offset_reset' => 'earliest',
+                                'offset' => 0,
+                                'handler' => ProductConsumer::class,
+                                'timeout' => 20000,
+                                'middlewares' => [],
+                            ],
+                        ],
+                        'required_acknowledgment' => true,
+                        'is_async' => true,
+                        'max_poll_records' => 500,
+                        'flush_attempts' => 10,
+                        'middlewares' => [],
+                        'timeout' => 20000,
+                    ]
+                ]
+            ]
+        );
+    }
+
+    protected function runTheLowLevelConsumer(): void
+    {
+        $this->artisan(
+            'kafka:consume',
+            [
+                'topic' => 'low_level',
+                'consumer_group' => 'test-consumer-group',
+                '--partition' => 0,
+                '--offset' => 2,
+                '--timeout' => 20000,
+                '--times' => 2,
+            ]
+        );
+    }
+
+    private function haveSomeRandomMessagesProduced(): string
+    {
         $record = str_random(10);
         $producer = app(ProductHasChanged::class, compact('record'));
 
-        // Expectations
+        Metamorphosis::produce($producer);
+        Metamorphosis::produce($producer);
+
+        return $record;
+    }
+
+    private function myMessagesHaveBeenLogged(string $recordMessage): void
+    {
         Log::shouldReceive('info')
             ->withAnyArgs();
 
         Log::shouldReceive('alert')
-            ->with($record)
+            ->with($recordMessage)
             ->twice();
-
-        // Actions
-        Metamorphosis::produce($producer);
-        Metamorphosis::produce($producer);
-        $this->artisan('kafka:consume', [
-            'topic' => 'default',
-            'consumer_group' => 'test-consumer-group',
-            '--timeout' => 20000,
-            '--times' => 2,
-        ]);
     }
 
-    public function testShouldRunProducerWithLowLevelConsumer(): void
+    private function haveTwoMessagesProducedBefore(string $recordMessage): void
     {
-        // Set
-        config(['kafka.topics' => [
-            'low_level' => [
-                'topic_id' => 'low_level',
-                'broker' => 'default',
-                'consumer_groups' => [
-                    'test-consumer-group' => [
-                        'offset_reset' => 'earliest',
-                        'offset' => 0,
-                        'handler' => ProductConsumer::class,
-                        'timeout' => 20000,
-                        'middlewares' => [],
-                    ],
-                ],
-                'required_acknowledgment' => true,
-                'is_async' => true,
-                'max_poll_records' => 500,
-                'flush_attempts' => 10,
-                'middlewares' => [],
-                'timeout' => 20000,
-            ]
-        ]]);
-        $record = 'first record';
-        $otherRecord = 'second record';
+        $this->produceRecordMessage($recordMessage);
+    }
+
+    private function andHaveMoreTwoMessagesProducedLater(string $recordMessage): void
+    {
+        $this->produceRecordMessage($recordMessage);
+    }
+
+    private function produceRecordMessage(string $record): string
+    {
         $producer = app(ProductHasChanged::class, compact('record'));
         $producer->topic = 'low_level';
 
-        $producer2 = app(ProductHasChanged::class, ['record' => $otherRecord]);
-        $producer2->topic = 'low_level';
-
-        // Expectations
-        Log::shouldReceive('info')
-            ->withAnyArgs();
-
-        Log::shouldReceive('alert')
-            ->with($otherRecord)
-            ->twice();
-
-        // Actions
         Metamorphosis::produce($producer);
         Metamorphosis::produce($producer);
-        Metamorphosis::produce($producer2);
-        Metamorphosis::produce($producer2);
-        $this->artisan('kafka:consume', [
-            'topic' => 'low_level',
-            'consumer_group' => 'test-consumer-group',
-            '--partition' => 0,
-            '--offset' => 2,
-            '--timeout' => 20000,
-            '--times' => 2,
-        ]);
+
+        return $record;
+    }
+
+    private function mySecondMessageHaveBeenLogged(string $secondMessage): void
+    {
+        $this->myMessagesHaveBeenLogged($secondMessage);
     }
 }

fab8e53ff8d8c0c54408ae154a636ea7216de5ca
djonasm <davidjonasmarques@gmail.com>
2020-01-16 18:17:24 -0300

    improve menager tests

--- tests/Unit/ManagerTest.php
+++ tests/Unit/ManagerTest.php
@@ -30,4 +30,36 @@ class ManagerTest extends LaravelTestCase
         $this->assertInstanceOf(Log::class, $middlewares[0]);
         $this->assertInstanceOf(JsonDecode::class, $middlewares[1]);
     }
+
+    public function testShouldRemoveOldMiddlewareBeforeAddOthers(): void
+    {
+        // Set
+        $manager = new Manager();
+        $firstConfig = [
+            'topic' => 'products',
+            'middlewares' => [
+                Log::class,
+                app(JsonDecode::class),
+            ],
+            'other' => 'config',
+        ];
+        $secondConfig = [
+            'topic' => 'products',
+            'middlewares' => [
+                JsonDecode::class,
+            ],
+            'other' => 'config',
+        ];
+
+        // Actions
+        $manager->set($firstConfig);
+        $manager->set($secondConfig);
+        $other = $manager->get('other');
+        $middlewares = $manager->middlewares();
+
+        // Assertions
+        $this->assertSame('config', $other);
+        $this->assertInstanceOf(JsonDecode::class, $middlewares[0]);
+        $this->assertCount(1, $middlewares);
+    }
 }

b10e88f96b5347002e9859c29fe9116fb5f21f51
Fonseca <gdfonseca@leroymerlin.com.br>
2021-05-14 17:22:51 -0300

    fix message enconder tests

--- tests/Unit/Avro/Serializer/MessageEncoderTest.php
+++ tests/Unit/Avro/Serializer/MessageEncoderTest.php
@@ -18,20 +18,25 @@ class MessageEncoderTest extends LaravelTestCase
         $registry = m::mock(CachedSchemaRegistryClient::class);
         $serializer = new MessageEncoder($registry);
         $topic = 'my-topic';
-        $schema = new AvroSchema('array');
+        $schema = m::mock(Schema::class);
+        $avroSchema = new AvroSchema('array');
         $record = [];
         $id = 3;
 
         // Expectations
-        $registry->expects()
-            ->getSchemaId("{$topic}-value", $schema)
-            ->andReturn($id);
+        $schema->expects()
+            ->getSchemaId()
+            ->andReturn('my-topic');
+
+        $schema->expects()
+            ->getAvroSchema()
+            ->andReturn($avroSchema);
 
         // Actions
         $result = $serializer->encodeMessage($topic, $schema, $record);
 
         // Assertions
-        $this->assertSame("\x00\x00\x00\x00\x03\x00", $result);
+        $this->assertSame("\x00\x00\x00\x00\x00\x00", $result);
     }
 
     public function testShouldEncodeRecordWithSchemaIdRegisteringMissingSchemas(): void
@@ -40,45 +45,57 @@ class MessageEncoderTest extends LaravelTestCase
         $registry = m::mock(CachedSchemaRegistryClient::class);
         $serializer = new MessageEncoder($registry, ['register_missing_schemas' => true, 'default_encoding_format' => SchemaFormats::MAGIC_BYTE_SCHEMAID]);
         $topic = 'my-topic';
-        $schema = new AvroSchema('array');
+        $schema = m::mock(Schema::class);
+        $avroSchema = new AvroSchema('array');
         $record = [];
         $id = 3;
 
         // Expectations
-        $registry->expects()
-            ->getSchemaId("{$topic}-key", $schema)
-            ->andThrow(new RuntimeException());
+        $schema->expects()
+            ->getSchemaId()
+            ->andReturn('my-topic');
 
-        $registry->expects()
-            ->register("{$topic}-key", $schema)
-            ->andReturn($id);
+        $schema->expects()
+            ->getAvroSchema()
+            ->andReturn($avroSchema);
 
         // Actions
         $result = $serializer->encodeMessage($topic, $schema, $record, true, SchemaFormats::MAGIC_BYTE_SCHEMAID);
 
         // Assertions
-        $this->assertSame("\x00\x00\x00\x00\x03\x00", $result);
+        $this->assertSame("\x00\x00\x00\x00\x00\x00", $result);
     }
 
-    public function testShouldNotEncodeRecordWhenSchemaIsMissing(): void
+    public function testShouldEncodeRecordWithSubjectAndVersion(): void
     {
         // Set
         $registry = m::mock(CachedSchemaRegistryClient::class);
         $serializer = new MessageEncoder($registry);
         $topic = 'my-topic';
-        $schema = new Schema();
+        $subject = "{$topic}-value";
+        $schema = m::mock(Schema::class);
+        $avroSchema = new AvroSchema('array');
         $record = [];
+        $version = 5;
 
         // Expectations
-        $registry->expects()
-            ->getSchemaId("{$topic}-key", $schema)
-            ->andThrow(new RuntimeException('Whoops'));
+        $schema->expects()
+            ->getVersion()
+            ->andReturn('some-version');
 
-        $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('Whoops');
+        $schema->expects()
+            ->getSubject()
+            ->andReturn($subject);
+
+        $schema->expects()
+            ->getAvroSchema()
+            ->andReturn($avroSchema);
 
         // Actions
-        $serializer->encodeMessage($topic, $schema, $record, true, SchemaFormats::MAGIC_BYTE_SCHEMAID);
+        $result = $serializer->encodeMessage($topic, $schema, $record, false, SchemaFormats::MAGIC_BYTE_SUBJECT_VERSION);
+
+        // Assertions
+        $this->assertSame("\x01\x00\x00\x00\x0Emy-topic-value\x00\x00\x00\x00\x00", $result);
     }
 
     public function testShouldNotEncodeRecordWithInvalidFormat(): void
@@ -87,7 +104,8 @@ class MessageEncoderTest extends LaravelTestCase
         $registry = m::mock(CachedSchemaRegistryClient::class);
         $serializer = new MessageEncoder($registry);
         $topic = 'my-topic';
-        $schema = new AvroSchema('array');
+        $schema = m::mock(Schema::class);
+        $avroSchema = new AvroSchema('array');
         $record = [];
 
         // Expectations
@@ -98,75 +116,35 @@ class MessageEncoderTest extends LaravelTestCase
         $serializer->encodeMessage($topic, $schema, $record, true, 2);
     }
 
-    public function testShouldEncodeRecordWithSubjectAndVersion(): void
-    {
-        // Set
-        $registry = m::mock(CachedSchemaRegistryClient::class);
-        $serializer = new MessageEncoder($registry);
-        $topic = 'my-topic';
-        $schema = new AvroSchema('array');
-        $record = [];
-        $version = 5;
-
-        // Expectations
-        $registry->expects()
-            ->getSchemaVersion("{$topic}-value", $schema)
-            ->andReturn($version);
-
-        // Actions
-        $result = $serializer->encodeMessage($topic, $schema, $record, false, SchemaFormats::MAGIC_BYTE_SUBJECT_VERSION);
-
-        // Assertions
-        $this->assertSame("\x01\x00\x00\x00\x0Emy-topic-value\x00\x00\x00\x05\x00", $result);
-    }
-
     public function testShouldEncodeRecordWithSubjectAndVersionRegisteringMissingSchemas(): void
     {
         // Set
         $registry = m::mock(CachedSchemaRegistryClient::class);
         $serializer = new MessageEncoder($registry, ['register_missing_schemas' => true]);
         $topic = 'my-topic';
-        $schema = new AvroSchema('string');
+        $subject = "{$topic}-value";
+        $schema = m::mock(Schema::class);
+        $avroSchema = new AvroSchema('string');
         $record = 'my awesome message';
         $version = 5;
 
         // Expectations
-        $registry->expects()
-            ->getSchemaVersion("{$topic}-key", $schema)
-            ->andThrow(new RuntimeException());
+        $schema->expects()
+            ->getVersion()
+            ->andReturn('some-version');
 
-        $registry->expects()
-            ->register("{$topic}-key", $schema);
+        $schema->expects()
+            ->getSubject()
+            ->andReturn($subject);
 
-        $registry->expects()
-            ->getSchemaVersion("{$topic}-key", $schema)
-            ->andReturn($version);
+        $schema->expects()
+            ->getAvroSchema()
+            ->andReturn($avroSchema);
 
         // Actions
         $result = $serializer->encodeMessage($topic, $schema, $record, true, SchemaFormats::MAGIC_BYTE_SUBJECT_VERSION);
 
         // Assertions
-        $this->assertSame("\x01\x00\x00\x00\fmy-topic-key\x00\x00\x00\x05\$my awesome message", $result);
-    }
-
-    public function testShouldNotEncodeRecordWithSubjectAndVersionWhenSchemaIsMissing(): void
-    {
-        // Set
-        $registry = m::mock(CachedSchemaRegistryClient::class);
-        $serializer = new MessageEncoder($registry);
-        $topic = 'my-topic';
-        $schema = new AvroSchema('array');
-        $record = [];
-
-        // Expectations
-        $registry->expects()
-            ->getSchemaVersion("{$topic}-key", $schema)
-            ->andThrow(new RuntimeException('Whoops'));
-
-        $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('Whoops');
-
-        // Actions
-        $serializer->encodeMessage($topic, $schema, $record, true, SchemaFormats::MAGIC_BYTE_SUBJECT_VERSION);
+        $this->assertSame("\x01\x00\x00\x00\x0Emy-topic-value\x00\x00\x00\x00\$my awesome message", $result);
     }
 }

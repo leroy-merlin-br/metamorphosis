4a5298fa5cf2a877db4bc88f29e31171d37b135b
Diego Felix <diegoflx.oliveira@gmail.com>
2021-06-08 09:28:28 -0300

    docs(auth): explain sasl ssl mechanisms

--- src/Authentication/SASLAuthentication.php
+++ src/Authentication/SASLAuthentication.php
@@ -21,6 +21,10 @@ class SASLAuthentication implements AuthenticationInterface
     private function authenticate(): void
     {
         $this->conf->set('security.protocol', ConfigManager::get('auth.type'));
+
+        // The mechanisms key is optional when configuring this kind of authentication
+        // If the user does not specify the mechanism, the default will be 'PLAIN'.
+        // But, to make config more clear, we are asking the user every time.
         $this->conf->set('sasl.mechanisms', ConfigManager::get('auth.mechanisms'));
         $this->conf->set('sasl.username', ConfigManager::get('auth.username'));
         $this->conf->set('sasl.password', ConfigManager::get('auth.password'));

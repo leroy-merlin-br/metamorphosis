015431e4a3bbbd2cef822cd659bf19ec0b3b9375
djonasm <davidjonasmarques@gmail.com>
2021-09-30 15:49:27 -0300

    fix: add ssl verify parameter

--- src/Connectors/Consumer/Config.php
+++ src/Connectors/Consumer/Config.php
@@ -29,6 +29,7 @@ class Config extends AbstractConfig
         'consumer_group' => 'required|string',
         'connections' => 'required|string',
         'url' => 'string',
+        'verify_ssl' => 'boolean',
         'auth' => 'array',
         'request_options' => 'array',
         'auto_commit' => 'boolean',

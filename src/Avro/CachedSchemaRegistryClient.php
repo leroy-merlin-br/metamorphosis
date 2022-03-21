b91b3873226ee6ebc08caaea7cc697916b4c3240
hcdias <huferreira@leroymerlin.com.br>
2022-03-21 15:25:33 -0300

    chore: remove inutilized interfaces

--- src/Avro/CachedSchemaRegistryClient.php
+++ src/Avro/CachedSchemaRegistryClient.php
@@ -5,7 +5,7 @@ namespace Metamorphosis\Avro;
 use AvroSchemaParseException;
 use RuntimeException;
 
-class CachedSchemaRegistryClient implements CachedSchemaRegistryClientInterface
+class CachedSchemaRegistryClient
 {
     private Client $client;
 
@@ -20,7 +20,7 @@ class CachedSchemaRegistryClient implements CachedSchemaRegistryClientInterface
      */
     private array $subjectVersionToSchema = [];
 
-    public function __construct(AvroClientInterface $client)
+    public function __construct(Client $client)
     {
         $this->client = $client;
     }

70cc41931a8336a11071da19f3d8bd2a7b0367b7
hcdias <huferreira@leroymerlin.com.br>
2022-03-24 14:44:51 -0300

    chore: remove annotation

--- src/Facades/Metamorphosis.php
+++ src/Facades/Metamorphosis.php
@@ -2,11 +2,7 @@
 namespace Metamorphosis\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Metamorphosis\TopicHandler\Producer\HandlerInterface;
 
-/**
- * @method static void produce(HandlerInterface $producerHandler)
- */
 class Metamorphosis extends Facade
 {
     protected static function getFacadeAccessor()
